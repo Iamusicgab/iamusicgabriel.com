fefd015be3ede2fc89fbcfb7514b3695c272b267
BuildTools <unconfigured@null.spigotmc.org>
2022-11-29 08:29:25 +0800

    First Development for Major Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './*.html',
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: true, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
