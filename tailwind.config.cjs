690ab623876115e6e84d3e15069f5f561fc783b1
Gabriel Gonzales <gfgonzales@donbosco.edu.ph>
2023-09-26 11:51:38 +0800

    Astro Configuration with Partial HTML Ref

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      byellow: "#FADF0B",
+      bpink: "#FF00F5",
+      blightblue: "#7DF9FF",
+      borange: "#FF4911",
+      bblue: "#3300FF",
+      blightviolet: "#C4A1FF",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Lexend Mega", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
