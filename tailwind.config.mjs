ed63a5ba8c2d57fceb4cbd5b354f87ed98c6faf9
Gabriel Gonzales <gfgonzales@donbosco.edu.ph>
2023-12-31 03:00:59 +0800

    Color imports

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,22 @@
 import daisyui from "daisyui";
-
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		colors: {
+			byellow: "#FADF0B",
+			bpink: "#FF00F5",
+			blightblue: "#7DF9FF",
+			borange: "#FF4911",
+			bblue: "#3300FF",
+			blightviolet: "#C4A1FF",
+		},
+		extend: {
+			fontFamily: {
+				sans: ["Lexend Mega", ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 };
