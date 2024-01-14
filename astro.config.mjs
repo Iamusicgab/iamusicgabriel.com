80ae460922b2b9c5e412836cfd067f3d6de9a733
Gabriel Gonzales <gfgonzales@donbosco.edu.ph>
2024-01-14 15:37:04 +0800

    Server-Side Rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
+	output: "server",
 	adapter: vercel({
 		webAnalytics: {
 			enabled: true,
