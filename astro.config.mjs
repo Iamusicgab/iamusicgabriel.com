4ef31f38e99739b842588614fb67058dcb5720fb
Gabriel Gonzales <gfgonzales@donbosco.edu.ph>
2025-09-03 09:04:31 +0800

    SSR2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -14,4 +16,5 @@ export default defineConfig({
 
   integrations: [react()],
   output: "server",
+  adapter: vercel(),
 });
