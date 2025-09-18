5030154f586b3fe7009c1b0e52f4d409c71d57a4
Gabriel Alexis F. Gonzales <gfgonzales@donbosco.edu.ph>
2025-09-18 22:19:12 +0800

    Loading Screen

--- src/components/IdWithObserver.jsx
+++ src/components/IdWithObserver.jsx
@@ -11,7 +11,7 @@ export default function IdWithObserver() {
 	return (
 		<div
 			ref={ref}
-			class="h-120 md:h-175 w-full flex flex-col items-center relative"
+			class="h-120 md:h-175 w-full flex flex-col items-center relative cursor-move"
 		>
 			{inView && <MyId client:load />}
 			<div class="bg-yellow border-2 border-base-100 p-2 absolute flex flex-col items-center w-fit h-fit bottom-0 text-xs">
